750e779688cc672a001a02e7c4bfdd1b8d4ba411
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-06-28 15:12:09 +0900

    feat: key 상태 확인 요청 추가
    - key 상태 확인 요청 추가
    - DTO 생성
    - drop key 로직 수정

---
+++ src/key/dtos/key.status.response.dto.ts
@@ -0,0 +1,3 @@
+export class KeyStatusResponseDto {
+  key_status: boolean;
+}
--- src/key/key.service.ts
+++ src/key/key.service.ts
@@ -4,6 +4,7 @@ import { DOOR_STATUS } from "./dtos/door.status.enum";
 import { DropKeyRequestDto } from "./dtos/drop-key.request.dto";
 import { DropKeyResponseDto } from "./dtos/drop-key.response.dto";
 import { KEY_STATUS } from "./dtos/key.status.enum";
+import { KeyStatusResponseDto } from "./dtos/key.status.response.dto";
 import { TakeKeyRequestDto } from "./dtos/take-key.request.dto";
 import { TakeKeyResponseDto } from "./dtos/take-key.response.dto";
 
@@ -19,14 +20,13 @@ export class KeyService {
       takeKeyRequestDto.building_location,
     );
 
-    const keyStatus = await this.checkKeyStatus(rfidSerialNo);
+    const keyStatus = await this.checkDBKeyStatus(rfidSerialNo);
 
     if (keyStatus === KEY_STATUS.NOT_EXIST) {
       throw new NotFoundException("Key Not Exist");
     }
 
-    console.info("아두이노 통신을 진행합니다.");
-    console.info("DOOR 상태를 확인합니다.");
+    console.info("DOOR OPEN 진행 [MQTT]");
 
     await prisma.status.create({
       data: {
@@ -49,8 +49,21 @@ export class KeyService {
       dropKeyRequestDto.building_location,
     );
 
-    console.info("아두이노 통신을 진행합니다.");
-    console.info("RFID, DOOR 상태를 확인합니다.");
+    console.info("DB의 Key 상태 최신화 [MQTT]");
+
+    const keyStatus = await this.checkDBKeyStatus(rfidSerialNo);
+
+    if (keyStatus === KEY_STATUS.NOT_EXIST) {
+      throw new NotFoundException("Key Not Exist");
+    }
+
+    console.info("DB의 Door 상태를 최신 [MQTT]");
+
+    const doorStatus = await this.checkDBDoorStatus(rfidSerialNo);
+
+    if (doorStatus === DOOR_STATUS.OPEN) {
+      throw new NotFoundException("Door Not Closed");
+    }
 
     await prisma.status.create({
       data: {
@@ -65,16 +78,22 @@ export class KeyService {
     };
   }
 
-  async doorStatus(rfidSerialNo: string) {
-    const doorStatus = await this.checkDoorStatus(rfidSerialNo);
+  async keyStatus(rfidSerialNo: string): Promise<KeyStatusResponseDto> {
+    const keyStatus = await this.checkDBKeyStatus(rfidSerialNo);
 
-    if (doorStatus === DOOR_STATUS.CLOSE) {
-      return false;
+    if (keyStatus === KEY_STATUS.NOT_EXIST) {
+      return {
+        key_status: false,
+      };
     }
 
-    if (doorStatus === DOOR_STATUS.OPEN) {
-      return true;
+    if (keyStatus === KEY_STATUS.EXIST) {
+      return {
+        key_status: true,
+      };
     }
+
+    throw new NotFoundException("Key Status Check Error");
   }
 
   async findKey(roomNo: number, buildingLocation: string): Promise<string> {
@@ -95,7 +114,7 @@ export class KeyService {
     return rfidSerialNo.rfidSerialNo;
   }
 
-  async checkKeyStatus(rfidSerialNo: string) {
+  async checkDBKeyStatus(rfidSerialNo: string) {
     const keyStatus = await prisma.status.findFirst({
       where: {
         rfidSerialNo: rfidSerialNo,
@@ -115,7 +134,7 @@ export class KeyService {
     return keyStatus.keyStatus;
   }
 
-  async checkDoorStatus(rfidSerialNo: string) {
+  async checkDBDoorStatus(rfidSerialNo: string) {
     const keyStatus = await prisma.status.findFirst({
       where: {
         rfidSerialNo: rfidSerialNo,
