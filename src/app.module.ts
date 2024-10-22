af693ffda23dfcc889b221c3202137b8e24b37dd
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-10-22 19:56:37 +0900

    add env set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { KeyModule } from "./key/key.module";
 
 @Module({
-  imports: [KeyModule],
+  imports: [
+    KeyModule,
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
